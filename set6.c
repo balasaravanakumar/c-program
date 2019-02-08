e1caf981a7b6b39b96bba5a5ec5fc37fc220666e
balasaravanakumar <47448076+balasaravanakumar@users.noreply.github.com>
2019-02-08 19:23:54 +0530

    Create set6.c

---
+++ set6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter a year: ");
+    scanf("%d",&a);
+
+    if(a%4 == 0)
+    {
+        if(a%100 == 0)
+        {              
+            
+            printf("%d is a leap year.",a);
+        }
+            else
+                printf("%d is a leap year.",a);
+        
+    }
+    else
+        printf("%d is not a leap year.",a);
+    
+    return 0;
+}
